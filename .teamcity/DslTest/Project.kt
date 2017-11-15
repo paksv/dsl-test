7271cfba43873662174e8f5140cb93a8a4ece564
Sergey Pak <paksv@yandex.ru>
2017-11-15 16:08:31 +0100

    inlined conf

--- .teamcity/DslTest/Project.kt
+++ .teamcity/DslTest/Project.kt
@@ -57,4 +57,22 @@ object Project : Project({
             maven {}
         }
     }))
+
+    buildType(BuildType({
+        uuid = "af3d7f42-b37d-41f5-84c7-a7ce4a728ad9"
+        id = "DslTest_Another"
+        name = "Another - deployment"
+        description = "test deployment conf"
+
+        enablePersonalBuilds = false
+        type = BuildTypeSettings.Type.DEPLOYMENT
+        maxRunningBuilds = 1
+
+        dependencies {
+            dependency("Temporary") {
+                snapshot {
+                }
+            }
+        }
+    }))
 })
