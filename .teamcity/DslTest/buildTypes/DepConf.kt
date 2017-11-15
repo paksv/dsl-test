76f4be090efefe918cc00fda19d0d36c24e4563a
Sergey Pak <paksv@yandex.ru>
2017-11-15 14:25:02 +0100

    name, id, uuid

--- .teamcity/DslTest/buildTypes/DepConf.kt
+++ .teamcity/DslTest/buildTypes/DepConf.kt
@@ -4,6 +4,9 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.script
 
 object DepConf : BuildType({
+    id="DepConf"
+    uuid = id
+    name = "Dependent Conf"
     steps {
         script {
             scriptContent = "echo script content"
