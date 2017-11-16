6e242b06587a335c95cf7d501297938b19370875
s <s@TeamCity>
2017-11-16 16:02:24 +0100

    TeamCity change in 'Dsl Test' project: triggers of 'Test All Runners and Settings' build configuration were updated

--- .teamcity/DslTest/patches/buildTypes/95f81f8c-07df-4eec-8c07-82437512e0aa.kts
+++ .teamcity/DslTest/patches/buildTypes/95f81f8c-07df-4eec-8c07-82437512e0aa.kts
@@ -1,9 +1,11 @@
 package DslTest.patches.buildTypes
 
+import .ScheduleTrigger
 import jetbrains.buildServer.configs.kotlin.v2017_2.*
 import jetbrains.buildServer.configs.kotlin.v2017_2.BuildType
 import jetbrains.buildServer.configs.kotlin.v2017_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2017_2.triggers.VcsTrigger
+import jetbrains.buildServer.configs.kotlin.v2017_2.triggers.schedule
 import jetbrains.buildServer.configs.kotlin.v2017_2.triggers.vcs
 import jetbrains.buildServer.configs.kotlin.v2017_2.ui.create
 
@@ -45,6 +47,16 @@ create("68ed399e-3cdc-4ffd-b638-e13a0b5b709f", BuildType({
             quietPeriodMode = VcsTrigger.QuietPeriodMode.USE_DEFAULT
             triggerRules = "asdasdsa"
             groupCheckinsByCommitter = true
+        }
+        schedule {
+            schedulingPolicy = cron {
+            }
+            triggerRules = "agvvbb"
+            triggerBuild = onWatchedBuildChange {
+                buildType = "DepConf"
+                watchedBuildRule = ScheduleTrigger.WatchedBuildRule.LAST_SUCCESSFUL
+            }
+            param("dayOfWeek", "Sunday")
         }
     }
 }))
