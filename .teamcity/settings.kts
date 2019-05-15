eb47b53f381a7c487338c284f39bdc51d7a4dfc3
sergey.pak <paksv@yandex.ru>
2019-05-15 14:44:52 +0200

    deps

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,6 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
+import jetbrains.buildServer.configs.kotlin.v2018_2.ui.findProjectFeature
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -25,127 +26,42 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 
 version = "2018.2"
 
+val deps = ArrayList<BuildType>()
+
 project {
     description = "Lots of DSL objects in here"
 
-    buildType(AllBuilds)
-    buildType(Bt3)
-    buildType(Bt2)
-    buildType(Bt1)
-    buildType(Bt5)
-    buildType(Bt4)
+    for (i in 0..20){
+        val bt = BuildType {
+            id ( "BT_$i")
+            name = "BT $i"
+
+            steps {
+                script {
+                    scriptContent = """
+                sleep 5
+                echo Hello BT $i
+            """.trimIndent()
+                }
+            }
+
+        }
+        deps.add(bt);
+        buildType(bt)
+    }
+    buildType(BuildType({
+        id("AllBuilds")
+        name = "AllBuilds"
+
+        type = BuildTypeSettings.Type.COMPOSITE
+
+        dependencies {
+            deps.forEach {
+                snapshot(it){
+                    reuseBuilds = ReuseBuilds.NO
+
+                }
+            }
+        }
+    }))
 }
-
-object AllBuilds : BuildType({
-    name = "AllBuilds"
-
-    type = BuildTypeSettings.Type.COMPOSITE
-
-    dependencies {
-        snapshot(Bt1) {
-            reuseBuilds = ReuseBuilds.NO
-        }
-        snapshot(Bt2) {
-            reuseBuilds = ReuseBuilds.NO
-        }
-        snapshot(Bt3) {
-            reuseBuilds = ReuseBuilds.NO
-        }
-        snapshot(Bt4) {
-            reuseBuilds = ReuseBuilds.NO
-        }
-        snapshot(Bt5) {
-            reuseBuilds = ReuseBuilds.NO
-        }
-    }
-})
-
-object Bt1 : BuildType({
-    name = "BT 1"
-    paused = true
-
-    steps {
-        script {
-            scriptContent = """
-                sleep 5
-                echo Hello BT 1
-            """.trimIndent()
-        }
-    }
-
-    requirements {
-        contains("system.cloud.profile_id", "kube")
-    }
-})
-
-object Bt2 : BuildType({
-    name = "BT 2"
-    paused = true
-
-    steps {
-        script {
-            scriptContent = """
-                sleep 5
-                echo Hello BT 2
-            """.trimIndent()
-        }
-    }
-
-    requirements {
-        contains("system.cloud.profile_id", "kube")
-    }
-})
-
-object Bt3 : BuildType({
-    name = "BT 3"
-    paused = false
-
-    steps {
-        script {
-            scriptContent = """
-                sleep 5
-                echo Hello BT 3
-            """.trimIndent()
-        }
-    }
-
-    requirements {
-        contains("system.cloud.profile_id", "kube")
-    }
-})
-
-object Bt4 : BuildType({
-    name = "BT 4"
-    paused = true
-
-    steps {
-        script {
-            scriptContent = """
-                sleep 5
-                echo Hello BT 4
-            """.trimIndent()
-        }
-    }
-
-    requirements {
-        contains("system.cloud.profile_id", "kube")
-    }
-})
-
-object Bt5 : BuildType({
-    name = "BT 5"
-    paused = false
-
-    steps {
-        script {
-            scriptContent = """
-                sleep 5
-                echo Hello BT 5
-            """.trimIndent()
-        }
-    }
-
-    requirements {
-        contains("system.cloud.profile_id", "kube")
-    }
-})
