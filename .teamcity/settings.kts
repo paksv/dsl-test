e62e95201c14872bcaee8c6c23986f0621655ead
sergey.pak <paksv@yandex.ru>
2019-05-19 16:46:15 +0200

    cloud profile -> DSL

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -31,37 +31,41 @@ val deps = ArrayList<BuildType>()
 project {
     description = "Lots of DSL objects in here"
 
-    for (i in 0..80){
-        val bt = BuildType {
-            id ( "BT_$i")
-            name = "BT $i"
-
-            steps {
-                script {
-                    scriptContent = """
-                sleep 5
-                echo Hello BT $i
-            """.trimIndent()
-                }
-            }
-
+    features {
+        feature {
+            id = "PROJECT_EXT_28"
+            type = "CloudImage"
+            param("use-spot-instances", "true")
+            param("security-group-ids", "sg-904db8ff,")
+            param("profileId", "amazon-30")
+            param("user-tags", "extra=tag")
+            param("agent_pool_id", "-2")
+            param("image-instances-limit", "2")
+            param("subnet-id", "subnet-17f8f17c")
+            param("ebs-optimized", "false")
+            param("instance-type", "c5d.large")
+            param("amazon-id", "ami-0583c0af8cebe48fe")
+            param("source-id", "ami-0583c0af8cebe48fe")
         }
-        deps.add(bt);
-        buildType(bt)
+        feature {
+            id = "amazon-30"
+            type = "CloudProfile"
+            param("profileServerUrl", "")
+            param("secure:access-id", "credentialsJSON:c14340bf-706c-4e1a-8ea7-49abde9cdb08")
+            param("total-work-time", "")
+            param("description", "")
+            param("cloud-code", "amazon")
+            param("enabled", "true")
+            param("max-running-instances", "3")
+            param("agentPushPreset", "")
+            param("profileId", "amazon-30")
+            param("name", "Dublin - ein")
+            param("next-hour", "")
+            param("secure:secret-key", "credentialsJSON:5397cc0f-9bc1-4a06-9729-1135042241b9")
+            param("region", "eu-west-1")
+            param("terminate-idle-time", "30")
+            param("not-checked", "")
+        }
     }
-    buildType(BuildType({
-        id("AllBuilds")
-        name = "AllBuilds"
 
-        type = BuildTypeSettings.Type.COMPOSITE
-
-        dependencies {
-            deps.forEach {
-                snapshot(it){
-                    reuseBuilds = ReuseBuilds.NO
-
-                }
-            }
-        }
-    }))
 }
