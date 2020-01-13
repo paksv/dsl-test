76c1de44aa2695b5cc9f65e748a5d4481f4bc283
s <s@TeamCity>
2020-01-13 20:46:10 +0100

    TeamCity change in 'New DSL' project: Connection updated

--- .teamcity/patches/projects/_Self.kts
+++ .teamcity/patches/projects/_Self.kts
@@ -20,7 +20,7 @@ changeProject(DslContext.projectId) {
                 param("aws.access.key.id", "aaasdas")
                 param("aws.credentials.type", "aws.access.keys")
                 param("aws.region.name", "eu-west-1")
-                param("registryId", "123412315")
+                param("registryId", "123412314")
                 param("providerType", "AmazonDocker")
             }
         }
